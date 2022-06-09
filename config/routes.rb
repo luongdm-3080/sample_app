1fc8045528e6af280a1723a979522ffa5b1bdb42
duong.minh.luong <duong.minh.luong@sun-asterisk.com>
2022-06-09 16:20:42 +0700

    Chapter 12: Password reset

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,11 @@ Rails.application.routes.draw do
     get "/contact", to: "static_pages#contact"
     get "/signup", to: "users#new"
     post "/signup", to: "users#create"
-    resources :users
-    resources :account_activations, only: :edit
     get "/login", to: "sessions#new"
     post "/login", to: "sessions#create"
     delete "/logout", to: "sessions#destroy"
+    resources :users
+    resources :account_activations, only: :edit
+    resources :password_resets, only: %i(new create edit update)
   end
 end
